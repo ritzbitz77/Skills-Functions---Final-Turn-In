c59d891f3925fd2bd1f615c75765c667acb745ae
Ritu Virmani <rituvirmani@Ritus-MacBook-Air.local>
2016-10-03 23:38:44 -0700

    Completed assignment

--- assessment.py
+++ assessment.py
@@ -14,6 +14,27 @@
 
 #    If the user does not provide a tax rate it should default to 5% 
 
+def calculate_total_price(default_tax, state, cost):
+    """ Calculuate total price give tax amount, state and cost
+
+    The default tax for all states excluding CA is 5%, CA is 7%, no response defaults 5% """
+    
+    default_tax = .05
+
+    if state == "CA":
+        default_tax = .07
+
+    elif state == " ":
+        default_tax = .05
+
+    total_price = cost * (default_tax + 1)
+
+    return total_price
+
+print calculate_total_price(.05, "CA", 12)
+
+
+
 #####################################################################
 # PART TWO
 
@@ -39,6 +60,64 @@
 #        we're from the same place!", or "Hi 'full name here', where are you 
 #        from?" depending on what `is_hometown()` evaluates to.
 
+def is_berry(fruit_name):
+    """ This function will return a boolean of T/F depending on the type of berry."""
+    
+    if fruit_name == "strawberry":
+        return True
+    elif fruit_name == "blackberry":
+        return True
+    elif fruit_name == "cherry":
+        return True
+    else:
+        return False
+
+
+
+def shipping_cost(fruit_name):
+    """This function returns a 0 or 5 depending if fruit name is a berry."""
+    if is_berry(fruit_name):
+        return 0
+    else:
+        return 5
+
+print is_berry("cherry")
+print shipping_cost("apple")
+
+
+
+def is_hometown(town_name):
+    ''' This function determines if the town name is my home town'''
+
+    if town_name == "Silver Spring":
+        return True
+    else:
+        return False
+
+print is_hometown("Gaithersburg")
+
+
+
+def full_name(first_name, last_name):
+    '''This function will return a full name combining first and last names.'''
+
+    return first_name + " " + last_name
+
+print full_name("Ritu", "Virmani")
+
+
+
+def hometown_greeting(home_town, first_name, last_name):
+    '''This function returns a greeting depending on whether person is from hometown or not.'''
+
+    if is_hometown(home_town):
+        return "Hi, %s, we're from the same place!" % (full_name(first_name, last_name))
+    else:
+        return "Hi  %s', where are you from?" % (full_name(first_name, last_name))
+
+print hometown_greeting("Silver Spring", "Ritu", "Virmani")
+print hometown_greeting("Germantown", "Ritu", "Virmani")
+
 #####################################################################
 
 # PART THREE
@@ -52,5 +131,30 @@
 
 # 3. Make a function that takes in a number and a list of numbers. It should append
 #    the number to the list of numbers and return the list.
+
+def increment(x = 1):
+'''This function creates a function that takes an argument and adds it to the outer functions argument.'''
+
+    def add(y):
+        return x + y
+    
+    return add
+
+addfive = increment(x = 5)
+
+print addfive(y = 5)
+print addfive(y = 20)
+
+def awesome_digits(number, list_of_numbers):
+
+    list_of_numbers.append(number)
+    return list_of_numbers
+
+
+print awesome_digits(5, [7, 4, 5, 2])
+
+
+
+
 
 #####################################################################
